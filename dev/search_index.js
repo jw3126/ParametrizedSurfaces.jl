7b4d6ecdeea33898e1d9c88596e590ae0dcf5aa5
Documenter.jl <documenter@juliadocs.github.io>
2023-04-13 20:29:09 +0000

    build based on 4be1083

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ParametrizedSurfaces","category":"page"},{"location":"#ParametrizedSurfaces","page":"Home","title":"ParametrizedSurfaces","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ParametrizedSurfaces.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ParametrizedSurfaces]","category":"page"}]
+}
