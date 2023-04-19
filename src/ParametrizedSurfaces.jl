c94838eab882db0782aaecba7d36bbcfcf9adfb8
Jan Weidner <jw3126@gmail.com>
2023-04-19 20:34:58 +0200

    more tests and change principal curvature sign convention

--- src/ParametrizedSurfaces.jl
+++ src/ParametrizedSurfaces.jl
@@ -9,7 +9,7 @@ export shape_operator
 export principal_curvatures, gaussian_curvature, mean_curvature
 export surface_normal
 
-function first_fundamental_form(f, uv)
+function calc_EFG(f,uv)
     uv = float(uv)
     J = ForwardDiff.jacobian(f, uv)
     f_u = J[:,1]
@@ -17,15 +17,25 @@ function first_fundamental_form(f, uv)
     E = f_u⋅f_u
     F = f_u⋅f_v
     G = f_v⋅f_v
-    @SMatrix[E F; F G]
+    (;E,F,G)
+end
+
+function first_fundamental_form(f, uv)
+    (;E,F,G) = calc_EFG(f, uv)
+    return @SMatrix[E F; F G]
+end
+
+function calc_LMN(f, uv)
+    m = second_fundamental_form(f,uv)
+    L = m[1,1]
+    M = m[2,1]
+    N = m[2,2]
+    return (;L,M,N)
 end
 
 function second_fundamental_form(f, uv)
     uv = float(uv)
-    J = ForwardDiff.jacobian(f, uv)
-    f_u = J[:,1]
-    f_v = J[:,2]
-    n = normalize(cross(f_u, f_v))
+    n = surface_normal(f, uv)
     h = ForwardDiff.hessian(uv->f(uv)⋅n, uv)
     return h
 end
@@ -33,14 +43,36 @@ end
 function shape_operator(f, uv)
     F1 = first_fundamental_form(f, uv)
     F2 = second_fundamental_form(f, uv)
-    F2*inv(F1)
+    return -F2*inv(F1)
 end
 
-function gaussian_curvature(f, uv)
+function shape_operator_ad(f,uv)
+    J = ForwardDiff.jacobian(f, uv)
+    Jn = ForwardDiff.jacobian(uv) do uv_
+        surface_normal(f, uv_)
+    end
+    J \ Jn
+end
+
+function gaussian_curvature(f,uv)
+    (;L,M,N) = calc_LMN(f,uv)
+    (;E,F,G) = calc_EFG(f,uv)
+    K = (L*N - M^2) / (E*G - F^2)
+    return K
+end
+
+function gaussian_curvature_det(f, uv)
     det(shape_operator(f, uv))
 end
 
-function mean_curvature(f, uv)
+function mean_curvature(f,uv)
+    (;L,M,N) = calc_LMN(f,uv)
+    (;E,F,G) = calc_EFG(f,uv)
+    H = (G*L-2*F*M + E*N) / (2*(E*G-F^2))
+    return -H
+end
+
+function mean_curvature_tr(f, uv)
     tr(shape_operator(f,uv))/2
 end
 
@@ -68,5 +100,42 @@ function surface_normal(f, uv)
     n = cross_uv(f, uv)
     return normalize(n)
 end
+
+#################################################################################
+#### Example surfaces
+################################################################################
+struct Torus
+    r::Float64
+    R::Float64
+end
+function (torus::Torus)(uv)
+    (;r,R) = torus
+    γ,Γ = uv
+    sinγ,cosγ = sincos(γ)
+    sinΓ,cosΓ = sincos(Γ)
+    x=(R+r*cosγ)*cosΓ
+    y=(R+r*cosγ)*sinΓ
+    z=r*sinγ
+    @SVector[x,y,z]
+end
+
+struct Sphere
+    radius::Float64
+end
+function (sphere::Sphere)(uv)
+    (;radius) = sphere
+    θ,ϕ = uv
+    x = radius*sin(θ)*cos(ϕ)
+    y = radius*sin(θ)*sin(ϕ)
+    z = radius*cos(θ)
+    @SVector[x,y,z]
+end
+
+# TODO
+# struct Cone
+# end
+
+# struct Cylinder
+# end
 
 end#module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,54 +1,24 @@
 using ParametrizedSurfaces
+import ParametrizedSurfaces as PS
 using Test
 using StaticArrays
 import Random: Xoshiro
 using LinearAlgebra
+using CoordinateTransformations
+using Random
+using Rotations
 
-@testset "sphere" begin
-    function sphere10(uv)
-        u,v = uv
-        R=10.0
-        z = sqrt(R^2 - u^2 - v^2)
-        @SVector[u,v,z]
-    end
-
-    uv = @SVector[0.0,0]
-    @inferred first_fundamental_form(sphere10, uv)
-    @inferred second_fundamental_form(sphere10, uv)
-    @inferred shape_operator(sphere10,uv)
-    @inferred gaussian_curvature(sphere10, uv)
-    @inferred mean_curvature(sphere10, uv)
-    @inferred principal_curvatures(sphere10, uv)
-
-    @test shape_operator(sphere10, uv) ≈ [-0.1 0.0; 0.0 -0.1] # TODO sign correct?
-
-    @test principal_curvatures(sphere10,uv) ≈ eigvals(shape_operator(sphere10, uv))
-
-    rng = Xoshiro(23)
-    @test shape_operator(sphere10, randn(rng, 2)) ≈ shape_operator(sphere10, zeros(2))
-
-    uv = randn(rng, 2)
-    @test eigvals(shape_operator(sphere10, uv)) ≈ principal_curvatures(sphere10, uv)
-end
-
-@testset "cone" begin
-    function cone(uv)
-        u,v = uv
-        z = sqrt(u^2 + v^2)
-        @SVector[u,v,z]
-    end
-
-    @test shape_operator(cone, [1,0]) ≈ [0 0; 0 1/sqrt(2)]
-    @test principal_curvatures(cone, [1,0]) ≈ [0, 1/sqrt(2)]
-
-    rng = Xoshiro(42)
-    for _ in 1:10
-        uv = randn(rng, 2)
-        r = norm(uv)
-        @test eigvals(shape_operator(cone, uv)) ≈ [0, inv(sqrt(2)*r)]
-        m = shape_operator(cone, uv)
-        @test m*uv ≈ [0, 0] atol=1e-15
-    end
+@testset "Sphere" begin
+    radius = 10.0
+    sphere = PS.Sphere(radius)
+    rng = Xoshiro(2)
+    θ = 2pi*rand(rng)
+    ϕ = pi*rand(rng)
+    uv = @SVector[θ,ϕ]
+    pt = sphere(uv)
+    @test surface_normal(sphere, uv) ≈ normalize(pt)
+    @test principal_curvatures(sphere, uv) ≈ @SVector[1/radius, 1/radius]
+    @test shape_operator(sphere, uv) ≈ PS.shape_operator_ad(sphere, uv)
 end
 
 @testset "paraboloid" begin
@@ -57,34 +27,90 @@ end
         z = 8*(1/2)*u^2 + 5*(1/2)*v^2 + 6*u*v -10
         @SVector[u,v,z]
     end
-    @test shape_operator(paraboloid, [0,0]) ≈ [8 6; 6 5]
+    @test shape_operator(paraboloid, [0,0]) ≈ -[8 6; 6 5]
+
+    rng = Xoshiro(43)
+    # uv = 0.1*randn(rng,2)
+    uv = zeros(2)
+    @test mean_curvature(paraboloid, uv) ≈ PS.mean_curvature_tr(paraboloid, uv)
+    @test gaussian_curvature(paraboloid, uv) ≈ PS.gaussian_curvature_det(paraboloid, uv)
+
+    @test shape_operator(paraboloid, uv) ≈ PS.shape_operator_ad(paraboloid, uv)
+
+    K = gaussian_curvature(paraboloid, uv)
+    H = mean_curvature(paraboloid, uv)
+    k1, k2 = sort(principal_curvatures(paraboloid, uv))
+    @test k1 ≈ H - √(H^2 -K)
+    @test k2 ≈ H + √(H^2 -K)
+
+    rot = rand(rng, AngleAxis)
+    offset = SVector{3}(randn(rng, 3))
+    trafo = AffineMap(rot, offset)
+    @test principal_curvatures(paraboloid, uv) ≈ principal_curvatures(trafo∘paraboloid, uv)
+    @test surface_normal(trafo∘paraboloid, uv) ≈ rot*surface_normal(paraboloid, uv)
 end
 
-struct Torus
-    r::Float64
-    R::Float64
-end
-function (torus::Torus)(uv)
-    (;r,R) = torus
-    γ,Γ = uv
-    sinγ,cosγ = sincos(γ)
-    sinΓ,cosΓ = sincos(Γ)
-    x=(R+r*cosγ)*cosΓ
-    y=(R+r*cosγ)*sinΓ
-    z=r*sinγ
-    @SVector[x,y,z]
+@testset "Torus" begin
+    @test PS.Torus(1,2)([0,0]) ≈ [3,0,0]
+    @test PS.Torus(1,2)([0,pi]) ≈ [-3,0,0]
+    @test PS.Torus(1,2)([pi,0]) ≈ [1,0,0]
+    @test PS.Torus(1,2)([pi,pi]) ≈ [-1,0,0]
+    r = 1.3
+    R = 3.9
+    t = PS.Torus(r,R)
+    uv = randn(Xoshiro(345), 2)
+    @test principal_curvatures(t, [0.0,0.0]) ≈ [-1/r, -1/(r+R),]
+    @test principal_curvatures(t, [0.0,pi ]) ≈ [-1/r, -1/(r+R),]
+    @test principal_curvatures(t, [pi ,0.0]) ≈ [-1/r, 1/(R-r),]   
+    @test principal_curvatures(t, [pi ,pi])  ≈ [-1/r, 1/(R-r),]   
+    @test shape_operator(t, uv) ≈ PS.shape_operator_ad(t, uv)
 end
 
-@testset "torus" begin
-    @test Torus(1,2)([0,0]) ≈ [3,0,0]
-    @test Torus(1,2)([0,pi]) ≈ [-3,0,0]
-    @test Torus(1,2)([pi,0]) ≈ [1,0,0]
-    @test Torus(1,2)([pi,pi]) ≈ [-1,0,0]
-    r = 1.2
-    R = 2.8
-    t = Torus(r,R)
-    @test principal_curvatures(t, [0.0,0.0]) ≈ [1/(r+R), 1/r]
-    @test principal_curvatures(t, [0.0,pi ]) ≈ [1/(r+R), 1/r]
-    @test principal_curvatures(t, [pi ,0.0]) ≈ [-1/(R-r), 1/r]   
-    @test principal_curvatures(t, [pi ,pi])  ≈ [-1/(R-r), 1/r]   
+@testset "half sphere" begin
+    function half_sphere10(uv)
+        u,v = uv
+        R=10.0
+        z = sqrt(R^2 - u^2 - v^2)
+        @SVector[u,v,z]
+    end
+
+    uv = @SVector[0.0,0]
+    @inferred first_fundamental_form(half_sphere10, uv)
+    @inferred second_fundamental_form(half_sphere10, uv)
+    @inferred shape_operator(half_sphere10,uv)
+    @inferred gaussian_curvature(half_sphere10, uv)
+    @inferred mean_curvature(half_sphere10, uv)
+    @inferred principal_curvatures(half_sphere10, uv)
+
+    @test shape_operator(half_sphere10, uv) ≈ [0.1 0.0; 0.0 0.1]
+    @test shape_operator(half_sphere10, uv) ≈ PS.shape_operator_ad(half_sphere10, uv)
+
+    @test principal_curvatures(half_sphere10,uv) ≈ eigvals(shape_operator(half_sphere10, uv))
+
+    rng = Xoshiro(23)
+    @test shape_operator(half_sphere10, randn(rng, 2)) ≈ shape_operator(half_sphere10, zeros(2))
+    # uv = rand(rng,2)
+
+    uv = randn(rng, 2)
+    @test eigvals(shape_operator(half_sphere10, uv)) ≈ principal_curvatures(half_sphere10, uv)
+    @test shape_operator(half_sphere10, uv) ≈ PS.shape_operator_ad(half_sphere10, uv)
+end
+
+@testset "cone" begin
+    function cone(uv)
+        u,v = uv
+        z = sqrt(u^2 + v^2)
+        @SVector[u,v,z]
+    end
+    @test shape_operator(cone, [1,0]) ≈ [0 0; 0 -1/sqrt(2)]
+    @test principal_curvatures(cone, [1,0]) ≈ [-1/sqrt(2), 0.0]
+    rng = Xoshiro(42)
+    for _ in 1:10
+        uv = randn(rng, 2)
+        r = norm(uv)
+        @test eigvals(shape_operator(cone, uv)) ≈ [-inv(sqrt(2)*r), 0.0]
+        m = shape_operator(cone, uv)
+        @test m*uv ≈ [0, 0] atol=1e-15
+        @test shape_operator(cone, uv) ≈ PS.shape_operator_ad(cone, uv)
+    end
 end
